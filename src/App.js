8466f05e314803b6347ed953893c6f8b2d89bf07
Cagatay Sert <cagatay.sert5@gmail.com>
2019-03-26 02:56:28 +0300

    redux learning path

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import { updateUser } from './actions/user-action';
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.onUpdateUser = this.onUpdateUser.bind(this);
+  }
+
+  onUpdateUser() {
+    this.props.onUpdateUser('Tarık');
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2> {this.props.user}</h2>
+          <button onClick={this.onUpdateUser}>Change the name</button>
         </header>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToPros = (state, props) => {
+  return {
+    ...state,
+    myCount: props.count + 2
+  };
+};
+
+const mapDispatchToProps = {
+  onUpdateUser: updateUser
+}
+
+const mergeProps = (propsFromState, propsFromDispatch, ownProps) => {
+  console.log(propsFromState);
+  console.log(propsFromDispatch);
+  console.log(ownProps);
+  return {};
+}
+export default connect(mapStateToPros, mapDispatchToProps, mergeProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import userReducer from './reducers/userReducer';
+import productReducer from './reducers/productReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const rootReducer = combineReducers({
+    products: productReducer,
+    user: userReducer
+});
+
+const store = createStore(
+    rootReducer, {
+        products: [{
+            name: 'samsung',
+            type: 'tv'
+        }],
+        user: 'çağatay'
+    },
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(<Provider store={store}><App count={4} /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
